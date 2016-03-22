379b54084352ddcfa30e4541961fdc2825451b6e
Jared Bennett <jarbenne@hwdsb.on.ca>
2016-03-22 00:11:04 -0400

    Update hide_category.php
    Hide the category from Meta and the Category Sidebar

--- hide_category.php
+++ hide_category.php
@@ -25,3 +25,39 @@ return;
 $query->set( 'cat', '-' . $cat->term_id );
 }
 add_action( 'pre_get_posts', 'hwdsb_hide_exclude_from_home' );
+
+// Remove categories from post meta - shared on basicwp.com
+function the_category_filter($thelist,$separator=' ') {
+	if(!defined('WP_ADMIN')) {
+//list the category names to exclude
+		$exclude = array('hide');
+		$cats = explode($separator,$thelist);
+		$newlist = array();
+		foreach($cats as $cat) {
+			$catname = trim(strip_tags($cat));
+			if(!in_array($catname,$exclude))
+				$newlist[] = $cat;
+		}
+		return implode($separator,$newlist);
+	} else
+		return $thelist;
+}
+add_filter('the_category','the_category_filter',10,2);
+
+//* Remove categories from widgets - shared on https://wordpress.org/support/topic/excluding-categories-by-slug-from-the-category-widget
+
+function exclude_widget_categories( $args ){
+
+$excludes = array('hide'); //array with category slugs to be excluded
+$cat_ids = array();
+foreach( $excludes as $cat_slug ) {
+  $cat = get_term_by( 'slug', $cat_slug, 'category' );
+  if( $cat ) $cat_ids[] = $cat->term_id;
+}
+$exclude = implode( ',', $cat_ids ); // The IDs of the excluding categories
+if( $cat_ids ) $args["exclude"] = $exclude;
+
+return $args;
+}
+
+add_filter("widget_categories_args","exclude_widget_categories");
